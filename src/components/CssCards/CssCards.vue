25772411c75eb34b9b86d790bbb779ec89976f1c
a.azdakhov <a.azdakhov@sociallinks.io>
2024-04-09 21:44:26 +0200

    Config

---
+++ src/components/CssCards/CssCards.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div :class="$style.container">
+    <div :class="$style.desk"></div>
+  </div>
+</template>
+
+<style module>
+.container {
+  height: 100%;
+  background-color: #000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+}
+.desk {
+  max-width: 620px;
+  width: 100%;
+  padding: 10px;
+  background-color: #948e8e;
+}
+</style>
---
+++ src/components/TheCards/TheCards.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import CssCards from "@/components/CssCards/CssCards.vue";
+</script>
+
+<template>
+  <div :class="$style.container">
+    <CssCards />
+  </div>
+</template>
+
+<style module>
+.container {
+  height: 100%;
+}
+</style>
